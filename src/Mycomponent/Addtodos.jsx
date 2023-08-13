a21536fdd14540fa0434dcc607726b587628a493
Satyam Anand <satyam2001anand@gmail.com>
2023-08-13 16:51:58 +0530

    error resolved 1

--- src/Mycomponent/Addtodos.jsx
+++ src/Mycomponent/Addtodos.jsx
@@ -21,11 +21,11 @@ export const Addtodos = (props) => {
       <h3 className=' p-8 max-md:pt-5 font-medium text-2xl max-md:text-xl'>Add Your ToDo </h3>
       <form onSubmit={submit}>
         <div className=" flex items-center justify-center mt-5 max-md:ml-5">
-          <label htmlFor="exampleInputEmail1" className="text-3xl p-3 max-md:text-base max-md:p-1 max-sm:text-xs"><h4><strong>Todo Title :</strong> </h4></label>
+          <label htmlFor="exampleInputEmail1" className="text-3xl p-3 max-md:text-base max-md:p-1 max-sm:text-xs"><h4><strong>Todo Title</strong> </h4></label>
           <input  type="text"  vlaue={title} onChange={(event)=>{settitle(event.target.value)}} className=" bg-slate-500  max-md:w-[70%] max-sm:w-[50%] max-md:h-5 rounded-3xl p-3 text-black text-lg font-medium border-[3px] border-violet-600" id="tittle"  />
         </div>
         <div className="flex items-center justify-center mt-5  -max-md:ml-4">
-          <label htmlFor="exampleInputPassword1" className=" "><h5 className='text-xl p-3 max-md:text-sm max-md:p-1 max-sm:text-xs '>Todo Description :</h5></label>
+          <label htmlFor="exampleInputPassword1" className=" "><h5 className='text-xl p-3 max-md:text-sm max-md:p-1 max-sm:text-xs '>Todo Description</h5></label>
           <input type="text" value={desc} onChange={(event)=>{setdesc(event.target.value)}} className=" bg-slate-500  w-[60%] max-sm:w-[40%] max-md:h-5 rounded-3xl p-3 text-black text-lg font-normal border-[3px] border-violet-600" id="desc" />
         </div> 
 
