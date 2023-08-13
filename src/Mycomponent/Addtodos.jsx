f4081eb94709852ad22f94231a499724fa0dda29
Satyam Anand <satyam2001anand@gmail.com>
2023-08-13 16:28:03 +0530

    description changed

--- src/Mycomponent/Addtodos.jsx
+++ src/Mycomponent/Addtodos.jsx
@@ -24,10 +24,11 @@ export const Addtodos = (props) => {
           <label htmlFor="exampleInputEmail1" className="text-3xl p-3 max-md:text-base max-md:p-1 max-sm:text-xs"><h4><strong>Todo Title :</strong> </h4></label>
           <input  type="text"  vlaue={title} onChange={(event)=>{settitle(event.target.value)}} className=" bg-slate-500  max-md:w-[70%] max-sm:w-[50%] max-md:h-5 rounded-3xl p-3 text-black text-lg font-medium border-[3px] border-violet-600" id="tittle"  />
         </div>
-        <div className="flex items-center justify-center mt-5  -max-md:ml-5">
+        <div className="flex items-center justify-center mt-5  -max-md:ml-4">
           <label htmlFor="exampleInputPassword1" className="text-xl p-3 max-md:text-sm max-md:p-1 max-sm:text-xs"><h5>Todo Description :</h5></label>
           <input type="text" value={desc} onChange={(event)=>{setdesc(event.target.value)}} className=" bg-slate-500  w-[60%] max-sm:w-[40%] max-md:h-5 rounded-3xl p-3 text-black text-lg font-normal border-[3px] border-violet-600" id="desc" />
         </div> 
+
 
         <div className=' flex items-center justify-center'><button type="submit" className="bg-yellow-300 hover:bg-lime-500 hover:text-white hover:font-bold  border-4 border-red-200 m-5 py-2 px-8 rounded-3xl max-md:px-4 max-md:py-1 max-md:text-sm">Add-Todo</button></div>
       </form>
