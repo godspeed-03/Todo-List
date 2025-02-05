cf30939a0cae45595902e9a81c3f5e93ff9e6aa7
godspeed-03 <satyam2001anand@gmail.com>
2025-02-05 17:53:52 +0530

    fix: fixed typo in the title value & added strictly type check for title

--- src/Mycomponent/Addtodos.jsx
+++ src/Mycomponent/Addtodos.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
 
-export const Addtodos = (props) => {
-  const[title, settitle] = useState("");
-  const[desc, setdesc] = useState("");
+export const Addtodos = ({ addTodo }) => {
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("");
 
-  const submit =(event)=>{
+  const submit = (event) => {
     event.preventDefault();
-    if(title == ''){
+    if (title === '') {
       alert("Title can't be blank.");
-    }else{
-
-      props.addTodo(title, desc);
+    } else {
+      addTodo(title, desc);
       settitle("");
       setdesc("");
     }
+
 
   }
   return (
@@ -22,12 +22,12 @@ export const Addtodos = (props) => {
       <form onSubmit={submit}>
         <div className=" flex items-center justify-center mt-5 max-md:ml-5">
           <label htmlFor="exampleInputEmail1" className="text-3xl p-3 max-md:text-base max-md:p-1 max-sm:text-xs"><h4><strong>Todo Title</strong> </h4></label>
-          <input  type="text"  vlaue={title} onChange={(event)=>{settitle(event.target.value)}} className=" bg-slate-500  max-md:w-[70%] max-sm:w-[50%] max-md:h-5 rounded-3xl p-3 text-black text-lg font-medium border-[3px] border-violet-600" id="tittle"  />
+          <input type="text" value={title} onChange={(event) => { settitle(event.target.value) }} className=" bg-slate-500  max-md:w-[70%] max-sm:w-[50%] max-md:h-5 rounded-3xl p-3 text-black text-lg font-medium border-[3px] border-violet-600" id="tittle" />
         </div>
         <div className="flex items-center justify-center mt-5  -max-md:ml-4">
           <label htmlFor="exampleInputPassword1" className=" "><h5 className='text-xl p-3 max-md:text-sm max-md:p-1 max-sm:text-xs '>Todo Description</h5></label>
-          <input type="text" value={desc} onChange={(event)=>{setdesc(event.target.value)}} className=" bg-slate-500  w-[60%] max-sm:w-[40%] max-md:h-5 rounded-3xl p-3 text-black text-lg font-normal border-[3px] border-violet-600" id="desc" />
-        </div> 
+          <input type="text" value={desc} onChange={(event) => { setdesc(event.target.value) }} className=" bg-slate-500  w-[60%] max-sm:w-[40%] max-md:h-5 rounded-3xl p-3 text-black text-lg font-normal border-[3px] border-violet-600" id="desc" />
+        </div>
 
 
         <div className=' flex items-center justify-center'><button type="submit" className="bg-yellow-300 hover:bg-lime-500 hover:text-white hover:font-bold  border-4 border-red-200 m-5 py-2 px-8 rounded-3xl max-md:px-4 max-md:py-1 max-md:text-sm">Add-Todo</button></div>
